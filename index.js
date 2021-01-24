a01c9e6dbee94894a75e625119be70a5c2ffe023
jmerges bootcamp <jamesmerges1@gmail.com>
2021-01-23 21:40:53 -0800

    Inquirer setup

--- index.js
+++ index.js
@@ -1,1 +1,113 @@
 var inquirer = require("inquirer");
+var Employee = require("./lib/Employee");
+var Manager = require("./lib/Manager");
+var Intern = require("./lib/Intern");
+var Engineer = require("./lib/Engineer");
+
+var employees = [];
+
+function promptManager() {
+    inquirer.prompt([
+        {
+            message: "Please enter the employee manager's name",
+            name: "name"
+        },
+        {
+            message: "Please enter the manager's ID",
+            name: "ID"
+        },
+        {
+            message: "Please enter the manager's email",
+            name: "email"
+        },
+        {
+            message: "Please enter the manager's office number",
+            name: "officeNumber"
+        }
+    ]).then(data => {
+        var mngr = new Manager(data.name, data.ID, data.email, data.officeNumber);
+        employees.push(mngr);
+        menu();
+    });
+}
+
+function menu() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "menuChoice",
+            choices: ["Add an intern", "Add an engineer", "Finish building team"],
+            message: "Add intern, engineer, or finish building team"
+        }
+    ]).then(data => {
+        switch(data.menuChoice) {
+            case "Add an intern":
+                addIntern();
+                break;
+            case "Add an engineer":
+                addEngineer();
+                break;
+            case "Finish building team":
+                buildTeam();
+                break;
+            default:
+                buildTeam();
+        }
+    })
+}
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            message: "Please enter the intern's name",
+            name: "name"
+        },
+        {
+            message: "Please enter the intern's ID",
+            name: "ID"
+        },
+        {
+            message: "Please enter the intern's email",
+            name: "email"
+        },
+        {
+            message: "Please enter the intern's school",
+            name: "school"
+        }
+    ]).then(data => {
+        var intrn = new Intern(data.name, data.ID, data.email, data.school);
+        employees.push(intrn);
+        menu();
+    });
+}
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            message: "Please enter the engineer's name",
+            name: "name"
+        },
+        {
+            message: "Please enter the engineer's ID",
+            name: "ID"
+        },
+        {
+            message: "Please enter the engineer's email",
+            name: "email"
+        },
+        {
+            message: "Please enter the engineer's github",
+            name: "github"
+        }
+    ]).then(data => {
+        var engi = new Engineer(data.name, data.ID, data.email, data.github);
+        employees.push(engi);
+        menu();
+    });
+}
+
+function buildTeam() {
+    console.log(employees);
+}
+
+promptManager();
